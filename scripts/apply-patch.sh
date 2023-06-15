b6995218374f7ff740869b9e7b07edc6830f1161
lopestorm <guillopes2002@gmail.com>
2023-06-15 18:25:16 -0300

    Update apply-patch.sh

--- scripts/apply-patch.sh
+++ scripts/apply-patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd bootable/recovery-twrp
-git apply -v ../../device/Tecno/Camon18P/patches/0001-persist_settings.patch
+git apply -v ../../device/tecno/CH7n/patches/0001-persist_settings.patch
 cd ../..
 
 echo Patch applied successfully
