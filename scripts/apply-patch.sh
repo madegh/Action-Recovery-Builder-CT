2b179c5b5ea7ed12afa976eff20984e3eb927881
lopestorm <guillopes2002@gmail.com>
2023-06-17 13:19:25 -0300

    Update apply-patch.sh

--- scripts/apply-patch.sh
+++ scripts/apply-patch.sh
@@ -3,7 +3,7 @@
 cd workspace
 cd bootable_recovery_twrp
 git apply -v ../../device/tecno/CH7n/patches/persist_settings.patch
-git apply -v https://github.com/lopestom/Action-Recovery-Builder-CT/edit/main/scripts/persist_settings.patch
+git apply -v https://github.com/lopestom/Action-Recovery-Builder-CT/main/scripts/persist_settings.patch
 git apply -v /home/runner/work/Action-Recovery-Builder-CT/Action-Recovery-Builder-CT/workspace/device/tecno/CH7n/patches/persist_settings.patch
 cd ../..
 
